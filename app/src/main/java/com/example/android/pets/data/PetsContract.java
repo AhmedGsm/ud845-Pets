0ad0fc0e35cf8ebbf9e6bf2c8325111cab13453f
AhmedGsm2018 <Ahmed.gsm1983@gmail.com>
2019-06-12 19:10:00 +0100

    Set values with PetsContract class constants names

--- app/src/main/java/com/example/android/pets/data/PetsContract.java
+++ app/src/main/java/com/example/android/pets/data/PetsContract.java
@@ -4,15 +4,15 @@ import android.provider.BaseColumns;
 
 public final class PetsContract {
     private PetsContract(){}
-    // TABLE NAME
-    public static final String TABLE_NAME = "pets";
 
-    // GENDER CONSTANTS
-    public static final int PETS_GENDER_MALE = 1;
-    public static final int PETS_GENDER_FEMALE= 2;
-    public static final int PETS_GENDER_UNKNOWN = 0;
     //Class Entry
     public static  class PetsEntry implements BaseColumns {
+        // TABLE NAME
+        public static final String TABLE_NAME = "pets";
+        // GENDER CONSTANTS
+        public static final int PETS_GENDER_MALE = 1;
+        public static final int PETS_GENDER_FEMALE= 2;
+        public static final int PETS_GENDER_UNKNOWN = 0;
         public static final String COLUMN_PET_ID = BaseColumns._ID;
         public static final String COLUMN_PET_NAME = "name";
         public static final String COLUMN_PET_BREED = "breed";
