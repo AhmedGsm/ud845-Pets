6f495cc9840ece4acaa8b6dddb53ae8d239c4226
AhmedGsm2018 <Ahmed.gsm1983@gmail.com>
2019-06-23 02:15:25 +0100

    Add UriMatcher to Content provider

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -3,6 +3,7 @@ package com.example.android.pets.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
@@ -15,6 +16,21 @@ public class PetProvider extends ContentProvider {
     /** Tag for the log messages */
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
     private PetDbHelper dbHelper;
+    // URI matcher code for the content URI for the pats table
+    private static final int PETS = 100;
+    // URI matcher code for the content URI for the pats table
+    private static final int PETS_ID = 101;
+    //URI matcher object
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        sUriMatcher.addURI(PetsContract.CONTENT_AUTHORITY,PetsContract.PATH_PETS,PETS);
+        sUriMatcher.addURI(PetsContract.CONTENT_AUTHORITY,PetsContract.PATH_PETS + "/#",PETS_ID);
+
+
+    }
+
+
 
     /**
      * Initialize the provider and the database helper object.
