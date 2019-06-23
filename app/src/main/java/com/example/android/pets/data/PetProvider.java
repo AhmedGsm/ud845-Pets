9aab5484b9ec6ca9240ad1d0c7e7543d3fa180b6
AhmedGsm2018 <Ahmed.gsm1983@gmail.com>
2019-06-23 11:23:29 +0100

    Implement query method in Content Provider

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -1,7 +1,9 @@
 package com.example.android.pets.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import com.example.android.pets.data.PetsContract.PetsEntry;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
@@ -15,17 +17,17 @@ public class PetProvider extends ContentProvider {
 
     /** Tag for the log messages */
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
-    private PetDbHelper dbHelper;
+    private PetDbHelper mDbHelper;
     // URI matcher code for the content URI for the pats table
     private static final int PETS = 100;
     // URI matcher code for the content URI for the pats table
-    private static final int PETS_ID = 101;
+    private static final int PET_ID = 101;
     //URI matcher object
     private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-
+   //Static uri matcher
     static {
         sUriMatcher.addURI(PetsContract.CONTENT_AUTHORITY,PetsContract.PATH_PETS,PETS);
-        sUriMatcher.addURI(PetsContract.CONTENT_AUTHORITY,PetsContract.PATH_PETS + "/#",PETS_ID);
+        sUriMatcher.addURI(PetsContract.CONTENT_AUTHORITY,PetsContract.PATH_PETS + "/#",PET_ID);
 
 
     }
@@ -37,7 +39,7 @@ public class PetProvider extends ContentProvider {
      */
     @Override
     public boolean onCreate() {
-        dbHelper = new PetDbHelper(getContext());
+        mDbHelper = new PetDbHelper(getContext());
         return true;
     }
 
@@ -47,9 +49,44 @@ public class PetProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        return null;
-    }
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
 
+        // This cursor will hold the result of the query
+        Cursor cursor ;
+
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                // For the PETS code, query the pets table directly with the given
+                // projection, selection, selection arguments, and sort order. The cursor
+                // could contain multiple rows of the pets table.
+                cursor = database.query(PetsEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            case PET_ID:
+                // For the PET_ID code, extract out the ID from the URI.
+                // For an example URI such as "content://com.example.android.pets/pets/3",
+                // the selection will be "_id=?" and the selection argument will be a
+                // String array containing the actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in the selection
+                // arguments that will fill in the "?". Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+                selection = PetsEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                // This will perform a query on the pets table where the _id equals 3 to return a
+                // Cursor containing that row of the table.
+                cursor = database.query(PetsEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
+    }
     /**
      * Insert new data into the provider with the given ContentValues.
      */
